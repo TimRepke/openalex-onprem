7f2bdb6453eadb5b2727a2f80473330ba115ae84
Tim Repke <repke@mcc-berlin.net>
2024-01-19 18:47:45 +0100

    fix curl

--- openalex-ingest/update_solr.py
+++ openalex-ingest/update_solr.py
@@ -46,7 +46,7 @@ def update_solr(tmp_dir: Path,  # Directory where we can write temporary parsed 
                         (f'http://{settings.solr_host}:{settings.solr_port}'
                          f'/api/collections/{settings.solr_collection}/update/json?commit=true'),
                         '-H', 'Content-type:application/json',
-                        '--silent'
+                        '--silent',
                         '-T', str(out_file)])
 
         logging.info('Partition posted to solr!')
