ac602b22b268197fab1dfc345693fd34d0b00528
Tim Repke <repke@mcc-berlin.net>
2024-01-23 15:59:07 +0100

    fix index droppage

--- openalex-ingest/setup/pg_indices_drop.sql
+++ openalex-ingest/setup/pg_indices_drop.sql
@@ -1,12 +1,12 @@
-DROP INDEX IF EXISTS works_id_doi_idx;
-DROP INDEX IF EXISTS works_publication_year_idx;
+DROP INDEX IF EXISTS oa.openalex.works_id_doi_idx;
+DROP INDEX IF EXISTS oa.openalex.works_publication_year_idx;
 
-DROP INDEX IF EXISTS works_authorships_work_id_idx;
-DROP INDEX IF EXISTS works_authorships_author_id_idx;
+DROP INDEX IF EXISTS oa.openalex.works_authorships_work_id_idx;
+DROP INDEX IF EXISTS oa.openalex.works_authorships_author_id_idx;
 
-DROP INDEX IF EXISTS works_authorship_institutions_author_id_idx;
-DROP INDEX IF EXISTS works_authorship_institutions_work_id_idx;
-DROP INDEX IF EXISTS works_authorship_institutions_institution_id_idx;
+DROP INDEX IF EXISTS oa.openalex.works_authorship_institutions_author_id_idx;
+DROP INDEX IF EXISTS oa.openalex.works_authorship_institutions_work_id_idx;
+DROP INDEX IF EXISTS oa.openalex.works_authorship_institutions_institution_id_idx;
 
-DROP INDEX IF EXISTS works_locations_work_id_idx;
-DROP INDEX IF EXISTS works_locations_source_id_idx;
+DROP INDEX IF EXISTS oa.openalex.works_locations_work_id_idx;
+DROP INDEX IF EXISTS oa.openalex.works_locations_source_id_idx;
