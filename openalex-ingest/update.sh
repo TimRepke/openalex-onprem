271a621df1ed0e36f96a9de805ef6b233e84f200
Tim Repke <repke@mcc-berlin.net>
2024-02-01 20:15:04 +0100

    pimp solr

--- openalex-ingest/update.sh
+++ openalex-ingest/update.sh
@@ -10,9 +10,13 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 config_file="secret.env"
 sync_s3=false
 run_solr=false
+run_solr_import=false
+run_solr_tmp=false
 run_solr_clr=false
 run_solr_res=false
+run_solr_swp=false
 
+run_pg=false
 run_pg_flat=false      # flatten postgres files
 run_pg_drop_ind=false  # drop indices before import
 run_pg_drop_dat=false  # drop selected data before import
@@ -20,6 +24,7 @@ run_pg_drop_full=false # drop all data before import
 run_pg_import=false    # import data
 run_pg_ind=false       # build indices
 run_pg_clr=false       # drop tmp files
+run_pg_tmp=false       # Spin up pg cluster
 
 solr_skip_del="--skip-deletion"
 pg_skip_del="--skip-deletion"
@@ -30,44 +35,45 @@ preserve_ram="--preserve-ram"
 jobs=1
 
 with_sudo=""
+with_tmp=false
 
 # Function to display script usage
 usage() {
- echo "Usage: $0 TMP_DIR [OPTIONS]"
- echo "   TMP_DIR is the absolute path to a directory where we can temporarily put files"
- echo "Options:"
- echo " --config FILE   .env config destination"
- echo " --sync          Sync OpenAlex S3 bucket"
- echo " --solr          Update Solr collection"
- echo " --solr-res      Reset solr index (start fresh)"
- echo " --solr-del      Delete deprecated data in solr"
- echo " --solr-clr      Clean temporary data (solr)"
- echo " --pg-del-ind    Drop all indices in postgres to speed up import"
- echo " --pg-del-dat    Drop all data from postgres before import"
- echo " --pg-del-upd    Drop only deprecated data from postgres before import"
- echo " --pg-flat       Flatten files for postgres"
- echo " --pg            Run postgres import"
- echo " --pg-ind        Create postgres indices"
- echo " --pg-clr        Clean temporary data (postgres)"
- echo " --override      Ignore existing flattened files and override them"
- echo " --jobs N        Number of processes for parallel processing"
- echo " --use-ram       Will not try to preserve RAM for small performance boost"
- echo " --no-progress   Set this to shut up aws sync updates (spams the gitlab-ci log)"
- echo " --with-sudo     Run some operations with sudo prefix"
+ echo "Usage: $0 [OPTIONS]"
  echo ""
- echo " -h, --help      Display this help message"
+ echo "Options:"
+ echo " --config FILE       .env config destination"
+ echo ""
+ echo " --sync              Sync OpenAlex S3 bucket"
+ echo ""
+ echo " --solr              Run solr-related tasks"
+ echo " --solr-import       Update Solr collection"
+ echo " --solr-tmp          Spin up temporary solr instance for import"
+ echo " --solr-swp          Shut down temporary solr instance and transfer solr-home"
+ echo " --solr-res          Reset solr index (start fresh)"
+ echo " --solr-del          Delete deprecated data in solr"
+ echo " --solr-clr          Clean temporary data (solr)"
+ echo ""
+ echo " --pg                Run postgres-related tasks"
+ echo " --pg-del-ind        Drop all indices in postgres to speed up import"
+ echo " --pg-del-dat        Drop all data from postgres before import"
+ echo " --pg-del-upd        Drop only deprecated data from postgres before import"
+ echo " --pg-flat           Flatten files for postgres"
+ echo " --pg-import         Run postgres import"
+ echo " --pg-tmp            Spin up temporary PG cluster for import"
+ echo " --pg-ind            Create postgres indices"
+ echo " --pg-clr            Clean temporary data (postgres)"
+ echo " --override          Ignore existing flattened files and override them"
+ echo " --jobs N            Number of processes for parallel processing"
+ echo " --use-ram           Will not try to preserve RAM for small performance boost"
+ echo " --no-progress       Set this to shut up aws sync updates (spams the gitlab-ci log)"
+ echo " --with-sudo         Run some operations with sudo prefix"
+ echo " --with-tmp          Use tmp parameters where possible"
+ echo ""
+ echo " -h, --help          Display this help message"
 }
 
-if [ $# -lt 1 ]; then
-  usage
-fi
-
-# Fetch first parameter, as this is the working directory
-tmp_dir=$1
-shift
-if [ ! -d "$tmp_dir" ]; then
-  echo "The TMP_DIR='$tmp_dir' does not exist!"
-  echo ""
+if [ $# -lt 2 ]; then
   usage
   exit 1
 fi
@@ -88,6 +94,19 @@ while [ $# -gt 0 ]; do
       ;;
     --solr)
       run_solr=true
+      ;;
+    --pg)
+      run_pg=true
+      ;;
+    --solr-import)
+      run_solr_import=true
+      ;;
+    --solr-tmp)
+      with_tmp=true
+      run_solr_tmp=true
+      ;;
+    --solr-swp)
+      run_solr_swp=true
       ;;
     --solr-res)
       run_solr_res=true
@@ -112,7 +131,7 @@ while [ $# -gt 0 ]; do
     --pg-flat)
       run_pg_flat=true
       ;;
-    --pg)
+    --pg-import)
       run_pg_import=true
       ;;
     --pg-ind)
@@ -120,6 +139,9 @@ while [ $# -gt 0 ]; do
       ;;
     --pg-clr)
       run_pg_clr=true
+      ;;
+    --pg-tmp)
+      run_pg_tmp=true
       ;;
     --override)
       override="--override"
@@ -137,6 +159,9 @@ while [ $# -gt 0 ]; do
     --with-sudo)
       with_sudo="sudo"
       ;;
+    --with-tmp)
+      with_tmp=true
+      ;;
     *)
       echo "Invalid option: $1" >&2
       usage
@@ -151,6 +176,15 @@ export OA_CONFIG="${config_file}"
 # Load config parameters from env file
 # shellcheck source=default.env
 source "${OA_CONFIG}"
+
+
+if [ ! -d "OA_TMP_DIR" ]; then
+  echo "The TMP_DIR='$OA_TMP_DIR' does not exist!"
+  echo ""
+  usage
+  exit 1
+fi
+
 
 do_compile() {
   DIR_PRE=$(pwd)
@@ -179,41 +213,80 @@ echo "Last S2 sync: ${LAST_SYNC} (from ${OA_LAST_SYNC_FILE})"
 echo "Last update: ${LAST_UPDT_PG} (from ${OA_LAST_UPDATE_PG_FILE})"
 echo "Last update: ${LAST_UPDT_SOLR} (from ${OA_LAST_UPDATE_SOLR_FILE})"
 
-if [ -z "$OA_SKIP_SYNC_STAGE" ]; then
-  # =======================================================
-  # S3 bucket sync
-  # =======================================================
-  echo "-=# (1/3) S3 bucket sync #=-"
+# =======================================================
+# S3 bucket sync
+# =======================================================
+echo "-=# S3 bucket sync #=-"
 
-  if [ "$sync_s3" = true ] && [ "$TODAY" \> "$LAST_SYNC" ]; then
-    echo "Syncing openalex S3 bucket..."
-    # Go to snapshot directory
-    cd "$OA_SNAPSHOT" || exit
+if [ "$sync_s3" = true ] && [ "$TODAY" \> "$LAST_SYNC" ]; then
+  echo "Syncing openalex S3 bucket..."
+  # Go to snapshot directory
+  cd "$OA_SNAPSHOT" || exit
 
-    # Go one up again so that s3 can sync it
-    cd ..
+  # Go one up again so that s3 can sync it
+  cd ..
 
-    # Commission S3 sync
-    aws s3 sync "s3://openalex" "openalex-snapshot" --no-sign-request --delete "$show_aws_progress"
+  # Commission S3 sync
+  aws s3 sync "s3://openalex" "openalex-snapshot" --no-sign-request --delete "$show_aws_progress"
 
-    # Update group to openalex, so that everyone can read it later
-    $with_sudo chgrp -R openalex .
-    $with_sudo chmod -R 775 .
+  # Update group to openalex, so that everyone can read it later
+  $with_sudo chgrp -R openalex .
+  $with_sudo chmod -R 775 .
 
-    # Remember that we synced the snapshot
-    rm -f "$OA_LAST_SYNC_FILE"
-    echo "$TODAY" > "$OA_LAST_SYNC_FILE"
-  else
-    echo "Assuming the OpenAlex snapshot is up to date, not syncing!"
-  fi
+  # Remember that we synced the snapshot
+  rm -f "$OA_LAST_SYNC_FILE"
+  echo "$TODAY" > "$OA_LAST_SYNC_FILE"
+else
+  echo "Assuming the OpenAlex snapshot is up to date, not syncing!"
 fi
 
 
-if [ -z "$OA_SKIP_SOLR_STAGE" ]; then
+if [ "$run_solr" = true ]; then
   # =======================================================
   # Solr
   # =======================================================
-  echo "-=# (2/3) SOLR import #=-"
+  echo "-=# SOLR import #=-"
+  
+  if [ "$with_tmp" = true ]; then
+    echo "Going to use tmp connection info"
+    OA_SOLR_BIN="${OA_SOLR_BIN_TMP}"
+    OA_SOLR_HOME="${OA_SOLR_HOME_TMP}"
+    OA_SOLR_HOST="${OA_SOLR_HOST_TMP}"
+    OA_SOLR_PORT="${OA_SOLR_PORT_TMP}"
+    OA_SOLR_ZOO="${OA_SOLR_ZOO_TMP}"
+  else
+    echo "Going to use production connection info"
+    OA_SOLR_BIN="${OA_SOLR_BIN_PROD}"
+    OA_SOLR_HOME="${OA_SOLR_HOME_PROD}"
+    OA_SOLR_HOST="${OA_SOLR_HOST_PROD}"
+    OA_SOLR_PORT="${OA_SOLR_PORT_PROD}"
+    OA_SOLR_ZOO="${OA_SOLR_ZOO_PROD}"
+  fi
+  echo "Solr at $OA_SOLR_HOST:$OA_SOLR_PORT/$OA_SOLR_COLLECTION (zoo at $OA_SOLR_ZOO)"
+  echo "  - Binaries: $OA_SOLR_BIN"
+  echo "  - Home: $OA_SOLR_HOME"
+
+  if [ "$run_solr_tmp" = true ]; then
+    cd "$SCRIPT_DIR" || exit
+
+    echo "Prepping secondary solr instance at port ${OA_SOLR_PORT_TMP}"
+    echo "  - Using solr binaries at: ${OA_SOLR_BIN_TMP}"
+    echo "  - Production solr-home: ${OA_SOLR_HOME_PROD}"
+    echo "  - Temporary solr-home: ${OA_SOLR_HOME_TMP}"
+
+    echo "Making sure tmp solr instance is running..."
+    "${OA_SOLR_BIN}/solr" start -c -p "$OA_SOLR_BIN_TMP"
+
+    echo "Dropping solr collection..."
+    "${OA_SOLR_BIN}/solr" delete -c "$OA_SOLR_COLLECTION" -p "$OA_SOLR_BIN_TMP"  || echo "Collection '$OA_SOLR_COLLECTION' did not exist!"
+
+    echo "Creating empty solr collection..."
+    "${OA_SOLR_BIN}/solr" zk upconfig -d setup/solr_configset -n _openalex_conf -z "$OA_SOLR_HOST_TMP:$OA_SOLR_ZOO_TMP"
+    "${OA_SOLR_BIN}/solr" create -c "$OA_SOLR_COLLECTION" -n _openalex_conf -p "$OA_SOLR_PORT_TMP"
+
+    echo "Waiting a bit..."
+    sleep 5
+  fi
 
   if [ "$run_solr_res" = true ]; then
     cd "$SCRIPT_DIR" || exit
@@ -222,9 +295,9 @@ if [ -z "$OA_SKIP_SOLR_STAGE" ]; then
     "${OA_SOLR_BIN}/solr" delete -c "$OA_SOLR_COLLECTION" -p "$OA_SOLR_PORT"  || echo "Collection '$OA_SOLR_COLLECTION' did not exist!"
 
     echo "Creating empty solr collection..."
-  #  "${OA_SOLR_BIN}/solr" zk cp file:setup/solr_managed-schema.xml zk:/configs/._designer_openalex/managed-schema.xml -z "$OA_SOLR_HOST:$OA_SOLR_ZOO"
+    # "${OA_SOLR_BIN}/solr" zk cp file:setup/solr_managed-schema.xml zk:/configs/._designer_openalex/managed-schema.xml -z "$OA_SOLR_HOST:$OA_SOLR_ZOO"
+    # "${OA_SOLR_BIN}/solr" create -c "$OA_SOLR_COLLECTION" -d setup/solr_configset -n openalex_conf -p "$OA_SOLR_PORT"
     "${OA_SOLR_BIN}/solr" zk upconfig -d setup/solr_configset -n _openalex_conf -z "$OA_SOLR_HOST:$OA_SOLR_ZOO"
-  #  "${OA_SOLR_BIN}/solr" create -c "$OA_SOLR_COLLECTION" -d setup/solr_configset -n openalex_conf -p "$OA_SOLR_PORT"
     "${OA_SOLR_BIN}/solr" create -c "$OA_SOLR_COLLECTION" -n _openalex_conf -p "$OA_SOLR_PORT"
 
     echo "Waiting a bit..."
@@ -233,7 +306,7 @@ if [ -z "$OA_SKIP_SOLR_STAGE" ]; then
     echo "Keeping existing index untouched."
   fi
 
-  if [ "$run_solr" = true ]; then
+  if [ "$run_solr_import" = true ]; then
     # Go back to the script directory
     cd "$SCRIPT_DIR" || exit
 
@@ -242,9 +315,15 @@ if [ -z "$OA_SKIP_SOLR_STAGE" ]; then
 
     # Load our python environment
     source ../venv/bin/activate
-
+    
     echo "Running solr import..."
-    python update_solr.py "$solr_skip_del" --loglevel INFO "$tmp_dir/solr"
+    python update_solr.py --tmp-dir="$OA_TMP_DIR/solr" \
+                          --snapshot-dir="$OA_SNAPSHOT" \
+                          --solr-collection="$OA_SOLR_COLLECTION" \
+                          --solr-host="$OA_SOLR_HOST" \
+                          --solr-port="$OA_SOLR_PORT" \
+                          --last-solr-update="$LAST_UPDT_SOLR" \
+                          "$solr_skip_del" --loglevel=INFO
 
     # Leave python environment
     deactivate
@@ -255,19 +334,42 @@ if [ -z "$OA_SKIP_SOLR_STAGE" ]; then
   fi
 
   if [ "$run_solr_clr" = true ]; then
-    echo "Clearing $tmp_dir/solr"
-    rm -r "$tmp_dir/solr"
+    echo "Clearing $OA_TMP_DIR/solr"
+    rm -r "$OA_TMP_DIR/solr"
   else
-    echo "Leaving $tmp_dir/solr untouched"
+    echo "Leaving $OA_TMP_DIR/solr untouched"
+  fi
+
+  if [ "$run_solr_swp" = true ]; then
+    echo "Transferring data from secondary solr instance to production"
+    echo "  - Using production solr binaries at: ${OA_SOLR_BIN_PROD}"
+    echo "  - Using secondary solr binaries at: ${OA_SOLR_BIN_TMP}"
+    echo "  - Production solr-home: ${OA_SOLR_HOME_PROD}"
+    echo "  - Temporary solr-home: ${OA_SOLR_HOME_TMP}"
+
+    echo "Shutting down solr instances as to not confuse them too much"
+    "${OA_SOLR_BIN_TMP}/solr" stop -p "$OA_OA_SOLR_PORT_TMP"
+    "${OA_SOLR_BIN_PROD}/solr" stop -p "$OA_OA_SOLR_PORT_PROD"
+
+    echo "Copying solr-home folders"
+    rm -r "$OA_SOLR_HOME_PROD"
+    cp -r "$OA_SOLR_HOME_TMP/*" "$OA_SOLR_HOME_PROD"
+    $with_sudo chown -R solr:solr "$OA_SOLR_HOME_PROD"
+
+    echo "Making sure tmp solr instance is running..."
+    # FIXME: move all settings to solr.in.sh
+    sudo -u solr "${OA_SOLR_BIN_PROD}/solr" start -c -h "$OA_SOLR_HOST_PROD" -m 20g -s "$OA_SOLR_HOME_PROD" -Denable.packages=true -Dsolr.modules=sql,clustering -Dsolr.max.booleanClauses=4096
+
+    echo "NOTE: not deleting the temporary solr-home at ${OA_SOLR_HOME_TMP}"
   fi
 fi
 
 
-if [ -z "$OA_SKIP_PG_STAGE" ]; then
+if [ "$run_pg" = true ]; then
   # =======================================================
   # Postgres
   # =======================================================
-  echo "-=# (3/3) Postgres import #=-"
+  echo "-=# Postgres import #=-"
 
   # shellcheck disable=SC2034
   export PGPASSWORD="$OA_PG_PW"  # set for passwordless postgres
@@ -283,7 +385,7 @@ if [ -z "$OA_SKIP_PG_STAGE" ]; then
     source ../venv/bin/activate
 
     echo "Flattening files for postgres"
-    python update_postgres.py --loglevel INFO --parallelism "$jobs" "$preserve_ram" "$pg_skip_del" "$override" "$tmp_dir/postgres"
+    python update_postgres.py --loglevel INFO --parallelism "$jobs" "$preserve_ram" "$pg_skip_del" "$override" "$OA_TMP_DIR/postgres"
 
     # Leave python environment
     deactivate
@@ -301,15 +403,35 @@ if [ -z "$OA_SKIP_PG_STAGE" ]; then
     psql -f ./setup/pg_clear.sql -p "$OA_PG_PORT" -h "$OA_PG_HOST" -U "$OA_PG_USER" --echo-all -d "$OA_PG_DB"
   elif [ "$run_pg_drop_dat" = true ]; then
     echo "Deleting merged objects..."
-    cd "$tmp_dir" || exit
+    cd "$OA_TMP_DIR" || exit
     find ./postgres -name "*-merged_del.sql" -exec psql -f {} -p "$OA_PG_PORT" -h "$OA_PG_HOST" -U "$OA_PG_USER" --echo-all -d "$OA_PG_DB" \;
     echo "Deleting existing new objects..."
     find ./postgres -name "*-del.sql" -exec psql -f {} -p "$OA_PG_PORT" -h "$OA_PG_HOST" -U "$OA_PG_USER" --echo-all -d "$OA_PG_DB" \;
   fi
 
+  if [ "$run_pg_tmp" = true ]; then
+    echo "Spinning up temporary PG cluster..."
+    pg_createcluster 16 oaimport -p 5434 -d "${pg_tmp_data}" --start
+    # create schema
+    # create users
+    # grant permissions
+    # [run import]
+    # [build indices]
+    # sudo pg_dropcluster 16 main
+    # sudo systemctl stop postgresql
+    # Drop old directory
+    # sudo rm /var/lib/postgresql/16/main
+    # Move new directory
+    # sudo mv "${pg_tmp_data}" /var/lib/postgresql/16/main
+    # edit /etc/postgresql/14/main/postgresql.conf
+    #    -> data_directory = '/var/lib/postgresql/16/main'
+    # sudo pg_dropcluster 16 oaimport
+    # start service again
+  fi
+
   if [ "$run_pg_import" = true ]; then
     echo "Import new or updated objects"
-    cd "$tmp_dir" || exit
+    cd "$OA_TMP_DIR" || exit
     find ./postgres -name "*-cpy.sql" -exec psql -f {} -p "$OA_PG_PORT" -h "$OA_PG_HOST" -U "$OA_PG_USER" --echo-all -d "$OA_PG_DB" \;
 
     # Remember that we synced the snapshot
@@ -325,9 +447,8 @@ if [ -z "$OA_SKIP_PG_STAGE" ]; then
 
   if [ "$run_pg_clr" = true ]; then
     echo "Deleting all temporary flattened files and scripts"
-    rm -r "$tmp_dir/postgres"
+    rm -r "$OA_TMP_DIR/postgres"
   fi
 fi
 
-echo "All updates done!"
-echo "Remember to update the date in $OA_LAST_UPDTAE_FILE"
+echo "Script finished!"
--- openalex-ingest/update_solr.py
+++ openalex-ingest/update_solr.py
@@ -3,9 +3,9 @@ import subprocess
 from pathlib import Path
 
 import typer
+from typing_extensions import Annotated
 
-from shared.config import settings
-from shared.util import get_globs, get_ids_to_delete, batched
+from shared.util import get_globs
 from processors.solr.transform_partition import transform_partition
 
 
@@ -14,7 +14,12 @@ def name_part(partition: Path):
     return f'{update}-{partition.stem}'
 
 
-def update_solr(tmp_dir: Path,  # Directory where we can write temporary parsed partition files
+def update_solr(tmp_dir: Annotated[Path, typer.Option(help='Directory for temporary parsed partition files')],
+                snapshot_dir: Annotated[Path, typer.Option(help='Path to openalex snapshot from S3')],
+                solr_collection: Annotated[str, typer.Option(help='Name of the Solr collection')],
+                solr_host: Annotated[str, typer.Option(help='Solr hostname')],
+                solr_port: Annotated[int, typer.Option(help='Solr port')],
+                last_solr_update: Annotated[str, typer.Option(help='YYYY-MM-DD of when solr was last updated')],
                 skip_deletion: bool = False,
                 loglevel: str = 'INFO'):
     logging.basicConfig(format='%(asctime)s [%(levelname)s] %(name)s (%(process)d): %(message)s', level=loglevel)
@@ -24,10 +29,10 @@ def update_solr(tmp_dir: Path,  # Directory where we can write temporary parsed 
     logging.info('Please ensure you synced the snapshot via\n'
                  '   $ aws s3 sync "s3://openalex" "openalex-snapshot" --no-sign-request')
 
-    logging.info(f'Looking for files younger than "{settings.last_update_solr}" '
-                 f'in snapshot folder at "{settings.snapshot}"')
+    logging.info(f'Looking for files younger than "{last_solr_update}" '
+                 f'in snapshot folder at "{snapshot_dir}"')
 
-    works, merged = get_globs(settings.snapshot, settings.last_update_solr, 'work')
+    works, merged = get_globs(snapshot_dir, last_solr_update, 'work')
 
     logging.info(f'Looks like there are {len(works)} works partitions '
                  f'and {len(merged)} merged_ids partitions since last update.')
@@ -43,8 +48,8 @@ def update_solr(tmp_dir: Path,  # Directory where we can write temporary parsed 
 
         subprocess.run(['curl',
                         '-X', 'POST',
-                        (f'http://{settings.solr_host}:{settings.solr_port}'
-                         f'/api/collections/{settings.solr_collection}/update/json?commit=true'),
+                        (f'http://{solr_host}:{solr_port}'
+                         f'/api/collections/{solr_collection}/update/json?commit=true'),
                         '-H', 'Content-type:application/json',
                         '--silent',
                         '-T', str(out_file)])
@@ -54,24 +59,27 @@ def update_solr(tmp_dir: Path,  # Directory where we can write temporary parsed 
         # Cleaning up
         out_file.unlink()
 
-    if not skip_deletion and len(merged) > 0:
-        logging.info('Going to delete merged works objects in batches...')
-        for del_batch in batched(get_ids_to_delete(merged), 1000):
-            ids = '</id><id>'.join(del_batch)
-            payload = f'<delete><id>{ids}</id></delete>'
-
-            subprocess.run([settings.solr_bin / 'post',
-                            '-c', settings.solr_collection,
-                            '-commit', 'yes',
-                            '-host', settings.solr_host,
-                            '-port', str(settings.solr_port),
-                            '-d', payload])
-    else:
-        logging.info('Found no merged work objects since last update and/or was asked to skip deletions!')
-
     logging.info('Solr collection is up to date.')
-    logging.warning(f'Remember to update the date in "{settings.last_update_solr_file}"')
 
 
 if __name__ == "__main__":
     typer.run(update_solr)
+
+# Deprecated bit of code. Keeping it in case we ever want to use it again.
+# This will assume we keep the existing collection and override documents,
+# then we'll have to delete the ones that openalex deleted.
+#
+# if not skip_deletion and len(merged) > 0:
+#     logging.info('Going to delete merged works objects in batches...')
+#     for del_batch in batched(get_ids_to_delete(merged), 1000):
+#         ids = '</id><id>'.join(del_batch)
+#         payload = f'<delete><id>{ids}</id></delete>'
+#
+#         subprocess.run([settings.solr_bin / 'post',
+#                         '-c', solr_collection,
+#                         '-commit', 'yes',
+#                         '-host', settings.solr_host,
+#                         '-port', str(settings.solr_port),
+#                         '-d', payload])
+# else:
+#     logging.info('Found no merged work objects since last update and/or was asked to skip deletions!')
