92bcbd4160afd664e391011296e043584b4bc1d5
Tim Repke <repke@mcc-berlin.net>
2025-05-02 22:39:57 +0200

    fix setttings

--- meta_cache/handlers/wrappers/__init__.py
+++ meta_cache/handlers/wrappers/__init__.py
@@ -1,3 +1,4 @@
+from abc import ABCMeta
 from typing import Type, Generator
 
 from .dimensions import DimensionsWrapper
@@ -18,7 +19,8 @@ def get_wrapper(key: WrapperEnum) -> Type[AnyWrapper]:
 def get_wrappers(keys: list[WrapperEnum] | str | None) -> Generator[Type[AnyWrapper], None, None]:
     if keys is None:
         keys = [WrapperEnum.DIMENSIONS, WrapperEnum.SCOPUS]
-    if type(keys) == str:
+
+    if type(keys) == str or type(keys) == ABCMeta:
         keys = [keys]
 
     for key in keys:
