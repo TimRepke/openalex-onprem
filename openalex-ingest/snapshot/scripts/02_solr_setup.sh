08d2689f806d06a7069cb8ad9add1f0e8b8ee849
Tim Repke <repke@mcc-berlin.net>
2025-11-29 01:38:16 +0100

    fixes

--- openalex-ingest/snapshot/scripts/02_solr_setup.sh
+++ openalex-ingest/snapshot/scripts/02_solr_setup.sh
@@ -65,10 +65,10 @@ echo "  - Home: $NACSOS_OPENALEX__SOLR_HOME"
 
 echo "Making sure solr instance is running..."
 "${NACSOS_OPENALEX__SOLR_BIN}/solr" stop --port "$NACSOS_OPENALEX__SOLR_PORT" --host "$NACSOS_OPENALEX__SOLR_HOST" || echo "Tried to stop solr, but wasn't running"
-"${NACSOS_OPENALEX__SOLR_BIN}/solr" start -c --host "$NACSOS_OPENALEX__SOLR_HOST" --port "$NACSOS_OPENALEX__SOLR_PORT" --solr-home "$NACSOS_OPENALEX__SOLR_HOME"
+"${NACSOS_OPENALEX__SOLR_BIN}/solr" start -c --host "$NACSOS_OPENALEX__SOLR_HOST" --port "$NACSOS_OPENALEX__SOLR_PORT" --solr-home "$NACSOS_OPENALEX__SOLR_HOME" -Dconfigset.upload.enabled=false
 
 echo "Dropping solr collection..."
-"${NACSOS_OPENALEX__SOLR_BIN}/solr" delete --name "$NACSOS_OPENALEX__SOLR_COLLECTION" --solr-url "http://$NACSOS_OPENALEX__SOLR_HOST:$NACSOS_OPENALEX__SOLR_PORT"  || echo "Collection '$NACSOS_OPENALEX__SOLR_COLLECTION' did not exist!"
+"${NACSOS_OPENALEX__SOLR_BIN}/solr" delete --name "$NACSOS_OPENALEX__SOLR_COLLECTION" -f --solr-url "http://$NACSOS_OPENALEX__SOLR_HOST:$NACSOS_OPENALEX__SOLR_PORT"  || echo "Collection '$NACSOS_OPENALEX__SOLR_COLLECTION' did not exist!"
 
 echo "Creating empty solr collection..."
 "${NACSOS_OPENALEX__SOLR_BIN}/solr" zk upconfig --conf-dir "solr/solr_configset" --conf-name _openalex_conf --solr-url "http://$NACSOS_OPENALEX__SOLR_HOST:$NACSOS_OPENALEX__SOLR_ZOO_PORT"
