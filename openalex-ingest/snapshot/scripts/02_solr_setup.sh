526fd11497e6ee01e5a4accfe19a8fd2eba9d909
Tim Repke <repke@mcc-berlin.net>
2025-12-02 17:59:59 +0100

    schema

--- openalex-ingest/snapshot/scripts/02_solr_setup.sh
+++ openalex-ingest/snapshot/scripts/02_solr_setup.sh
@@ -74,7 +74,7 @@ echo "Dropping solr collection..."
 
 echo "Creating empty solr collection..."
 "${NACSOS_OPENALEX__SOLR_BIN}/solr" zk upconfig --conf-dir "solr/solr_configset" --conf-name openalex_schema --solr-url "http://$NACSOS_OPENALEX__SOLR_HOST:$NACSOS_OPENALEX__SOLR_ZOO_PORT"
-"${NACSOS_OPENALEX__SOLR_BIN}/solr" create --name "$NACSOS_OPENALEX__SOLR_COLLECTION" --conf-name openalex_schema --solr-url "http://$NACSOS_OPENALEX__SOLR_HOST:$NACSOS_OPENALEX__SOLR_ZOO_PORT"
+"${NACSOS_OPENALEX__SOLR_BIN}/solr" create --name "$NACSOS_OPENALEX__SOLR_COLLECTION" --conf-dir "solr/solr_configset" --conf-name openalex_schema --solr-url "http://$NACSOS_OPENALEX__SOLR_HOST:$NACSOS_OPENALEX__SOLR_ZOO_PORT"
 
 echo "Waiting a bit..."
 sleep 5
